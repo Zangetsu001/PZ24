c0974eb1608e5998e7bfb29d0991db86efdd2570
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-02-20 11:00:59 +0200

    Add files via upload

---
+++ Avto.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Avto
+    {
+        /// <summary>
+        /// Марка автомобиля
+        /// </summary>
+        private string brand;
+        /// <summary>
+        /// Цвет Автомобиля
+        /// </summary>
+        private string color;
+        /// <summary>
+        /// Скорость автомобиля
+        /// </summary>
+        private double skor;
+        /// <summary>
+        /// Конструктор без пармаметров
+        /// </summary>
+        public Avto(){}
+        /// <summary>
+        /// Конструктор с 1 параметром
+        /// </summary>
+        /// <param name="brand">Бренд</param>
+        public Avto(string brand)
+        {
+            this.brand = brand;
+        }
+        /// <summary>
+        /// Конструктор с 2 параметрами
+        /// </summary>
+        /// <param name="brand">Бренд</param>
+        /// <param name="color">Цвет</param>
+        public Avto(string brand, string color) : this(brand)
+        {
+            this.color = color;
+        }
+        /// <summary>
+        /// Конструктор с 3 параметрами
+        /// </summary>
+        /// <param name="brand">Бренд</param>
+        /// <param name="color">Цвет</param>
+        /// <param name="skor">Скорость</param>
+        public Avto(string brand, string color, double skor) : this(brand,color)
+        {
+            this.skor = skor;
+        }
+        /// <summary>
+        /// Бренд
+        /// </summary>
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+            set
+            {
+                brand = value;
+            }
+        } 
+        /// <summary>
+        /// Цвет
+        /// </summary>
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+        /// <summary>
+        /// Скорость
+        /// </summary>
+        public double Skor
+        {
+            get
+            {
+                return skor;
+            }
+            set
+            {
+                if (value < 20)
+                    skor = 20;
+                else if (value > 120)
+                    skor = 120;
+                else skor = value;
+            }
+        }
+        /// <summary>
+        /// Вывод
+        /// </summary>
+        public void Output()
+        {
+            Console.WriteLine($"Бренд: {brand}\n Цвет: {color}\n Скорость: {skor}");
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto1 = new Avto();
+            avto1.Brand = "Toyota";
+            avto1.Color = "Красный";
+            avto1.Skor = 100;
+            avto1.Output();
+            Console.WriteLine();
+
+            Avto avto2 = new Avto("Mersedes", "Красный", 60);
+            avto2.Output();
+            Console.WriteLine();
+
+            Console.Write("Введите марку автомобиля: ");
+            string brandInput = Console.ReadLine();
+            Console.Write("Введите цвет автомобиля: ");
+            string colorInput = Console.ReadLine();
+            Console.Write("Введите скорость автомобиля (20-120 км/ч): ");
+            int skorInput = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Бренд: {brandInput}\n Цвет: {colorInput}\n Скорость: {skorInput}");
+
+            Console.Read();
+        }
+    }
+}
